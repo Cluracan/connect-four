d90e496b4fb72785b22e71009a636faa6ffd30b1
cluracan1 <cluracan1@gmail.com>
2025-06-11 13:30:14 +0100

    extract canvas functions to useCanvas hook (some overlap between canvasses, but not enough to merge components

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,74 +1,34 @@
 import { useEffect, useRef, useState } from "react";
 import type { LocationData } from "../types/gameBoard.types";
+import { drawBoard, drawDiscs } from "../hooks/useCanvas";
 import {
   GAMEBOARD_HEIGHT,
   GAMEBOARD_WIDTH,
   MOBILE_DISC_RADIUS,
 } from "../constants";
+
 interface CanvasProps {
   locationData: LocationData | undefined;
 }
-let RADIUS = MOBILE_DISC_RADIUS;
-let CANVAS_WIDTH = GAMEBOARD_WIDTH;
-let CANVAS_HEIGHT = GAMEBOARD_HEIGHT + 1;
 
-const drawStones = (
-  locationData: LocationData,
-  context: CanvasRenderingContext2D
-) => {
-  locationData.forEach((stone) => {
-    context.fillStyle = stone.colour;
-    context.beginPath();
-    context.arc(
-      (3 * stone.col + 1.5) * RADIUS,
-      (3 * (6 - stone.height) + 1.5) * RADIUS,
-      RADIUS,
-      0,
-      2 * Math.PI
-    );
-    context.fill();
-  });
-};
-
-const drawBoard = (context: CanvasRenderingContext2D) => {
-  context.fillStyle = "blue";
-  context.beginPath();
-  for (let row = 0; row < CANVAS_HEIGHT; row++) {
-    if (row === 0) continue;
-    for (let col = 0; col < CANVAS_WIDTH; col++) {
-      context.arc(
-        (3 * col + 1.5) * RADIUS,
-        (3 * row + 1.5) * RADIUS,
-        RADIUS,
-        0,
-        2 * Math.PI
-      );
-      context.rect(
-        (col * 3 + 3) * RADIUS,
-        row * 3 * RADIUS,
-        -3 * RADIUS,
-        3 * RADIUS
-      );
-    }
-  }
-
-  context.fill();
-};
+const RADIUS = MOBILE_DISC_RADIUS;
+const CANVAS_WIDTH = GAMEBOARD_WIDTH;
+const CANVAS_HEIGHT = GAMEBOARD_HEIGHT + 1;
 
 const Canvas = ({ locationData }: CanvasProps) => {
+  console.log("CanvasRefresh");
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
 
-  console.log("CanvasRefresh");
   useEffect(() => {
     if (canvasRef.current) {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext("2d");
       if (ctx) {
         setContext(ctx);
-        drawBoard(ctx);
+        drawBoard(ctx, CANVAS_WIDTH, CANVAS_HEIGHT, RADIUS);
         if (locationData) {
-          drawStones(locationData, ctx);
+          drawDiscs(locationData, ctx, RADIUS);
         }
       }
     }
@@ -76,17 +36,12 @@ const Canvas = ({ locationData }: CanvasProps) => {
 
   useEffect(() => {
     if (context && locationData) {
-      drawStones(locationData, context);
+      drawDiscs(locationData, context, RADIUS);
     }
   }, [locationData]);
 
   return (
-    <canvas
-      ref={canvasRef}
-      width={7 * 3 * RADIUS}
-      height={7 * 3 * RADIUS}
-      style={{ background: "green" }}
-    />
+    <canvas ref={canvasRef} width={7 * 3 * RADIUS} height={7 * 3 * RADIUS} />
   );
 };
 
--- src/components/ClassicCanvas.tsx
+++ src/components/ClassicCanvas.tsx
@@ -1,115 +1,28 @@
 import { useEffect, useRef, useState } from "react";
 import type { LocationData } from "../types/gameBoard.types";
+import { drawBoard, drawDiscs, dropDisc } from "../hooks/useCanvas";
 import {
   GAMEBOARD_HEIGHT,
   GAMEBOARD_WIDTH,
   MOBILE_DISC_RADIUS,
 } from "../constants";
+
 interface CanvasProps {
   locationData: LocationData | undefined;
-
   handleClick: any;
   canClick: Boolean;
 }
-let RADIUS = MOBILE_DISC_RADIUS;
-let backgroundColour = "#242424";
-let CANVAS_WIDTH = GAMEBOARD_WIDTH;
-let CANVAS_HEIGHT = GAMEBOARD_HEIGHT + 1;
 
-const drawStones = (
-  locationData: LocationData,
-  context: CanvasRenderingContext2D
-) => {
-  locationData.forEach((stone) => {
-    context.fillStyle = stone.colour;
-    context.beginPath();
-    context.arc(
-      (3 * stone.col + 1.5) * RADIUS,
-      (3 * (6 - stone.height) + 1.5) * RADIUS,
-      RADIUS,
-      0,
-      2 * Math.PI
-    );
-    context.fill();
-  });
-};
-
-const drawBoard = (context: CanvasRenderingContext2D) => {
-  context.fillStyle = "blue";
-  context.beginPath();
-  for (let row = 0; row < CANVAS_HEIGHT; row++) {
-    if (row === 0) continue;
-    for (let col = 0; col < CANVAS_WIDTH; col++) {
-      context.arc(
-        (3 * col + 1.5) * RADIUS,
-        (3 * row + 1.5) * RADIUS,
-        RADIUS,
-        0,
-        2 * Math.PI
-      );
-      context.rect(
-        (col * 3 + 3) * RADIUS,
-        row * 3 * RADIUS,
-        -3 * RADIUS,
-        3 * RADIUS
-      );
-    }
-  }
-
-  context.fill();
-};
-
-const dropStone = (
-  canvas: HTMLCanvasElement,
-  context: CanvasRenderingContext2D,
-  locationData: LocationData,
-  setStoneDropping: React.Dispatch<React.SetStateAction<boolean>>
-) => {
-  let animationFrameID;
-  const oldLocationData = locationData.slice(0, locationData.length - 1);
-  const curStone = locationData[locationData.length - 1];
-  setStoneDropping(true);
-  const stone: {
-    x: number;
-    y: number;
-    vy: number;
-    colour: String;
-    draw: () => void;
-  } = {
-    x: (3 * curStone.col + 1.5) * RADIUS,
-    y: 1.5 * RADIUS,
-    vy: 1,
-    colour: curStone.colour,
-    draw() {
-      context.beginPath();
-      context.arc(this.x, this.y, RADIUS, 0, Math.PI * 2);
-      context.closePath();
-      context.fillStyle = `${this.colour}`;
-      context.fill();
-      drawBoard(context);
-    },
-  };
-  function render() {
-    if (stone.y <= (3 * (CANVAS_HEIGHT - curStone.height) + 1.5) * RADIUS) {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      stone.draw();
-      drawStones(oldLocationData, context);
-      stone.y += stone.vy;
-      stone.vy += 0.9;
-      animationFrameID = window.requestAnimationFrame(render);
-    } else {
-      drawStones(locationData, context);
-      setStoneDropping(false);
-    }
-  }
-  animationFrameID = window.requestAnimationFrame(render);
-};
+const RADIUS = MOBILE_DISC_RADIUS;
+const CANVAS_WIDTH = GAMEBOARD_WIDTH;
+const CANVAS_HEIGHT = GAMEBOARD_HEIGHT + 1;
 
 const ClassicCanvas = ({
   locationData,
   handleClick,
   canClick,
 }: CanvasProps) => {
+  console.log("CanvasRefresh");
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
   const [boundingRect, setBoundingRect] = useState({
@@ -119,7 +32,7 @@ const ClassicCanvas = ({
     height: 0,
   });
   const [stoneDropping, setStoneDropping] = useState(false);
-  console.log("CanvasRefresh");
+
   useEffect(() => {
     if (canvasRef.current) {
       const canvas = canvasRef.current;
@@ -128,47 +41,53 @@ const ClassicCanvas = ({
       const ctx = canvas.getContext("2d");
       if (ctx) {
         setContext(ctx);
-        drawBoard(ctx);
+        drawBoard(ctx, CANVAS_WIDTH, CANVAS_HEIGHT, RADIUS);
         if (locationData) {
-          drawStones(locationData, ctx);
+          drawDiscs(locationData, ctx, RADIUS);
         }
       }
     }
   }, []);
 
   useEffect(() => {
-    console.log({ locationData });
-
     if (canvasRef.current && context) {
       const canvas = canvasRef.current;
       if (locationData) {
-        console.log({ locationData });
-        dropStone(canvas, context, locationData, setStoneDropping);
+        //new disc has been played
+        dropDisc(
+          canvas,
+          context,
+          locationData,
+          setStoneDropping,
+          CANVAS_WIDTH,
+          CANVAS_HEIGHT,
+          RADIUS
+        );
       } else {
-        context.fillStyle = backgroundColour;
-        context.fillRect(0, 0, canvas.width, canvas.height);
-        drawBoard(context);
+        //reset => no location data
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        drawBoard(context, CANVAS_WIDTH, CANVAS_HEIGHT, RADIUS);
       }
     }
   }, [locationData]);
+
+  function calculateColumn(e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) {
+    return Math.floor(
+      (CANVAS_WIDTH * (e.clientX - boundingRect.left)) / boundingRect.width
+    );
+  }
 
   return (
     <>
       <canvas
         onClick={(e) => {
           if (canClick && !stoneDropping) {
-            handleClick(
-              Math.floor(
-                (CANVAS_WIDTH * (e.clientX - boundingRect.left)) /
-                  boundingRect.width
-              )
-            );
+            handleClick(calculateColumn(e));
           }
         }}
         ref={canvasRef}
         width={CANVAS_WIDTH * 3 * RADIUS}
         height={CANVAS_HEIGHT * 3 * RADIUS}
-        style={{ background: backgroundColour }}
       />
     </>
   );
