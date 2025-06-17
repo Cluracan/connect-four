52c62c4a1b7617cd836a7ddd2d320e8ca6388b79
cluracan1 <cluracan1@gmail.com>
2025-06-17 20:50:22 +0100

    locationData is null if empty

--- src/hooks/useCanvas.tsx
+++ src/hooks/useCanvas.tsx
@@ -1,4 +1,4 @@
-import { LocationData } from "../types/gameBoard.types";
+import { LocationData } from "../types.ts";
 
 const drawBoard = (
   context: CanvasRenderingContext2D,
@@ -35,18 +35,20 @@ const drawDiscs = (
   context: CanvasRenderingContext2D,
   radius: number
 ) => {
-  locationData.forEach((stone) => {
-    context.fillStyle = stone.colour;
-    context.beginPath();
-    context.arc(
-      (3 * stone.col + 1.5) * radius,
-      (3 * (6 - stone.height) + 1.5) * radius,
-      radius,
-      0,
-      2 * Math.PI
-    );
-    context.fill();
-  });
+  if (locationData) {
+    locationData.forEach((stone) => {
+      context.fillStyle = stone.colour;
+      context.beginPath();
+      context.arc(
+        (3 * stone.col + 1.5) * radius,
+        (3 * (6 - stone.height) + 1.5) * radius,
+        radius,
+        0,
+        2 * Math.PI
+      );
+      context.fill();
+    });
+  }
 };
 
 const dropDisc = (
@@ -59,45 +61,48 @@ const dropDisc = (
   radius: number
 ) => {
   let animationFrameID: number;
-  const oldLocationData = locationData.slice(0, locationData.length - 1);
-  const curStone = locationData[locationData.length - 1];
-  setStoneDropping(true);
-  const stone: {
-    x: number;
-    y: number;
-    vy: number;
-    colour: String;
-    draw: () => void;
-  } = {
-    x: (3 * curStone.col + 1.5) * radius,
-    y: 1.5 * radius,
-    vy: 1,
-    colour: curStone.colour,
-    draw() {
-      context.beginPath();
-      context.arc(this.x, this.y, radius, 0, Math.PI * 2);
-      context.closePath();
-      context.fillStyle = `${this.colour}`;
-      context.fill();
-      drawBoard(context, canvasWidth, canvasHeight, radius);
-    },
-  };
-  function render() {
-    if (stone.y <= (3 * (canvasHeight - curStone.height) + 1.5) * radius) {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      stone.draw();
-      drawDiscs(oldLocationData, context, radius);
+  if (locationData) {
+    console.log(locationData);
+    const oldLocationData = locationData.slice(0, locationData.length - 1);
+    const curStone = locationData[locationData.length - 1];
+    setStoneDropping(true);
+    const stone: {
+      x: number;
+      y: number;
+      vy: number;
+      colour: String;
+      draw: () => void;
+    } = {
+      x: (3 * curStone.col + 1.5) * radius,
+      y: 1.5 * radius,
+      vy: 1,
+      colour: curStone.colour,
+      draw() {
+        context.beginPath();
+        context.arc(this.x, this.y, radius, 0, Math.PI * 2);
+        context.closePath();
+        context.fillStyle = `${this.colour}`;
+        context.fill();
+        drawBoard(context, canvasWidth, canvasHeight, radius);
+      },
+    };
+    function render() {
+      if (stone.y <= (3 * (canvasHeight - curStone.height) + 1.5) * radius) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        stone.draw();
+        drawDiscs(oldLocationData, context, radius);
 
-      stone.y += stone.vy;
-      stone.vy += 0.9;
-      animationFrameID = window.requestAnimationFrame(render);
-    } else {
-      drawDiscs(locationData, context, radius);
-      setStoneDropping(false);
-      window.cancelAnimationFrame(animationFrameID);
+        stone.y += stone.vy;
+        stone.vy += 0.9;
+        animationFrameID = window.requestAnimationFrame(render);
+      } else {
+        drawDiscs(locationData, context, radius);
+        setStoneDropping(false);
+        window.cancelAnimationFrame(animationFrameID);
+      }
     }
+    animationFrameID = window.requestAnimationFrame(render);
   }
-  animationFrameID = window.requestAnimationFrame(render);
 };
 
 export { drawBoard, drawDiscs, dropDisc };
