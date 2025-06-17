b8dc10831d650d9e34a91ca53a386408009c39ae
cluracan1 <cluracan1@gmail.com>
2025-06-17 20:49:46 +0100

    arrow notation consistency

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,8 @@
 import { Settings } from "../pages/Settings";
 
+const Index = () => {
+  return <Settings />;
+};
 export const Route = createFileRoute({
   component: Index,
 });
-
-function Index() {
-  return <Settings />;
-}
